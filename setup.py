19d49f3b751306b849cf474f9ad847a6e3b8991c
Alejandro Angulo <aab.j13@gmail.com>
2016-12-30 10:56:50 -0800

    bumped version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-codesnip',
     install_requires=['pygments'],
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(),
     include_package_data=True,
     test_suite='runtests.runtests',
